db37c767e638689ec93c67f9976d69dc7e45a81f
Theo Schlossnagle <jesus@omniti.com>
2013-06-10 22:17:26 +0000

    fix from Andy for repeated characters

--- net_help.sh
+++ net_help.sh
@@ -28,7 +28,7 @@
 Ether(){
   /sbin/ifconfig -a | \
     /usr/bin/awk '/UP/{if($2 !~ /LOOPBACK/){iface=$1;}} /ether/{if(iface){print $2; exit;}}' | \
-    /bin/tr -s '[:lower:]' '[:upper:]' | \
+    /bin/tr '[:lower:]' '[:upper:]' | \
     /bin/sed -e 's/^/ 0/g;s/:/ 0/g; s/0\([0-9A-F][0-9A-F]\)/\1/g; s/ //g;'
 }
 
