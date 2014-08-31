fc987cc91d3d0d5dbed8aded740d521955e210b6
hal9k <hal9k-illumos@skynet.be>
2014-08-31 15:09:20 +0200

    SetHostname - Also update /etc/hosts
    Signed-off-by: hal9k <hal9k-illumos@skynet.be>

--- install_help.sh
+++ install_help.sh
@@ -176,6 +176,10 @@ SetHostname()
   log "Setting hostname: ${1}"
   /bin/hostname "$1"
   echo "$1" > $ALTROOT/etc/nodename
+  head -n 26 $ALTROOT/etc/hosts > /tmp/hosts
+  echo "::1\t\t$1" >> /tmp/hosts
+  echo "127.0.0.1\t$1" >> /tmp/hosts
+  cat /tmp/hosts > $ALTROOT/etc/hosts
 }
 
 SetTimezone()
