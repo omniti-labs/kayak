0088d70c1a61cfe62bce4f52d546d4e423480bbb
Dan McDonald <danmcd@omniti.com>
2015-03-25 16:28:37 -0400

    Update repo URLs for r151014

--- build_image.sh
+++ build_image.sh
@@ -30,9 +30,8 @@ fail() {
 }
 
 # NOTE --> The URL needs to be updated with every release.
-# Change "bloody" to whatever release the current branch is.
 PUBLISHER=omnios
-: ${PKGURL:=http://pkg.omniti.com/omnios/bloody}
+: ${PKGURL:=http://pkg.omniti.com/omnios/r151014}
 : ${GZIP_CMD:=gzip}
 SRCDIR=$(dirname $0)
 DIDWORK=0
